1f52f511a415ea3b286fc071cfbbf890fef1994f
sidhartha07 <sidharthajobies@gmail.com>
2025-10-24 20:21:58 +0530

    initial commit

---
+++ src/main/java/com/sidhartha/demojenkinsapp/DemoJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sidhartha.demojenkinsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidhartha/demojenkinsapp/DemoJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidhartha.demojenkinsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
